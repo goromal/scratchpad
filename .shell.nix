f5f3dafc05a7180bf5f08a1ef3187d55445efeba
Andrew Torgesen <atorgesen@anduril.com>
2024-06-01 11:33:28 -0700

    initial commit; launch works

---
+++ .shell.nix
@@ -0,0 +1,14 @@
+let
+  pkgs = import (builtins.fetchTarball "https://github.com/goromal/anixpkgs/archive/refs/heads/master.tar.gz") {};
+  py = pkgs.python310; # should match the version used with sage
+in pkgs.mkShell {
+  buildInputs = with pkgs; with py.pkgs; [
+    sage
+    py
+    numpy
+    scipy
+    geometry
+    find_rotational_conventions
+  ];
+}
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# Scratchpad
+
+A place for me to do math. Powered by [sage](https://doc.sagemath.org/) as well as my own [packages](https://goromal.github.io/anixpkgs/).
+
+## Get Started
+
+1. Install Nix and Direnv.
+2. Run `launch` in this directory.
