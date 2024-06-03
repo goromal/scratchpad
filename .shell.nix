1ed2671dc17425eb9f37926c3ebb0b1f8a6b170a
Andrew Torgesen <andrew.torgesen@gmail.com>
2024-06-02 21:28:54 -0700

    get mesh-plotter animations working (#1)

--- .shell.nix
+++ .shell.nix
@@ -1,5 +1,5 @@
 let
-  pkgs = import (builtins.fetchTarball "https://github.com/goromal/anixpkgs/archive/refs/heads/master.tar.gz") {};
+  pkgs = import (builtins.fetchTarball "https://github.com/goromal/anixpkgs/archive/refs/tags/v5.19.2.tar.gz") {};
   py = pkgs.python310; # should match the version used with sage
 in pkgs.mkShell {
   buildInputs = with pkgs; with py.pkgs; [
@@ -8,6 +8,9 @@ in pkgs.mkShell {
     numpy
     scipy
     geometry
+    pysignals
+    pyceres_factors
+    mesh-plotter
     find_rotational_conventions
     (octaveFull.withPackages (ps: with ps; [
       control
